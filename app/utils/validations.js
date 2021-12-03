872ee1ab4f30d0b342ff4e1489bc8fa1aa9b33b2
Kennedy Ngure <kngure@fintech-group.com>
2021-12-03 14:33:07 +0300

    added report release files

--- app/utils/validations.js
+++ app/utils/validations.js
@@ -1,4 +1,24 @@
 const {ReasonPhrases, StatusCodes} = require('http-status-codes');
+const redis = require("redis");
+const PORT = process.env.PORT || 4000;
+const PORT_REDIS = process.env.PORT || 6379;
+const app = express();
+const redisClient = redis.createClient(PORT_REDIS);
+
+const set = (key, value) => {
+    redisClient.set(key, JSON.stringify(value));
+}
+
+const get = (req, res, next) => {
+    let key = req.route.path;
+    redisClient.get(key, (error, data) => {
+        if (error) res.status(400).send(err);
+        if (data !== null) res.status(200).send(JSON.parse(data));
+        else next();
+    });
+}
+
+
 
 exports.errorResponse = (err, res, objectName) => {
     console.log(err.name);
@@ -22,7 +42,7 @@ exports.errorResponse = (err, res, objectName) => {
             error: ReasonPhrases.CONFLICT,
             message: err.message || " Association Error on " + objectName + "."
         });
-    } else if (err.message === "NotFound"  || err.name === "NotFoundError") {
+    } else if (err.message === "NotFound" || err.name === "NotFoundError") {
         return res.status(StatusCodes.NOT_FOUND).send({
             status: err.statusCode || StatusCodes.NOT_FOUND,
             error: ReasonPhrases.NOT_FOUND,
@@ -37,7 +57,11 @@ exports.errorResponse = (err, res, objectName) => {
 }
 
 exports.showAll = (res, model) => {
+
+    set(req.route.path, json);
+
     return res.status(StatusCodes.OK).send({
+
         status: StatusCodes.OK,
         message: ReasonPhrases.OK,
         data: model
@@ -71,4 +95,5 @@ exports.showOne = (res, model) => {
         data: model
     });
 }
+
 
